1bfa0f75a616a087d8173e0362e2ab7e0a1e95f2
Mikael Carlsson <micke@radiosyd.se>
2026-08-07 20:52:39 +0200

    Show number of components and projects

--- HTML/include/footer.php
+++ HTML/include/footer.php
@@ -1,18 +1,23 @@
-
 <!-- FILE: FOOTER.PHP - CSS from STYLES.CSS -->
 
 <div id="copyText">
     <div class="leftBox">
-        <div>© 2010 - <?php echo date('Y'); ?> ecDB - Created by <a href="http://nilsf.se">Nils Fredriksson</a> revised by Pete Willard- <a href="contact.php">Contact us</a> - <a href="terms.php">Terms & Privacy</a></div>
-        <div class="stats">
-        
-			<?php //$components = mysqli_num_rows(mysqli_query($connection,"SELECT id FROM data")); echo $components; ?>
-			<span class="boldText">components </span>and
-
-			<?php //$projects = mysqli_num_rows(mysqli_query($connection,"SELECT project_id FROM projects")); echo $projects; ?>
-			<span class="boldText">projects</span>.
-
-        </div>
+        <div>© 2010 - <?php echo date('Y'); ?> ecDB - Created by Nils Fredriksson revised by Pete Willard and modified by Mikael Karlsson</div>
+<?php
+    if(isset($owner)) {
+        echo '<div class="stats">';
+        $components = mysqli_num_rows(mysqli_query($connection,"SELECT id FROM data"));
+        echo $components;
+        echo '<span class="boldText"> components </span>and ';
+        $projects = mysqli_num_rows(mysqli_query($connection,"SELECT project_id FROM projects"));
+        echo $projects;
+        if($projects < 2 ) {
+            echo '<span class="boldText"> project</span>.';
+        } else {
+            echo '<span class="boldText"> projects</span>.';
+        }
+        echo '</div>';
+    }
+?>
     </div>
-    
 </div>
